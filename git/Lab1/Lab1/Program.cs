08cf4ff9e5542b47702fe29cb8c08cd6d6d0bc7c
Darya Kopytok <dariakopitok4@gmail.com>
2018-09-24 21:30:28 +0300

    Initial project

--- git/Lab1/Lab1/Program.cs
+++ git/Lab1/Lab1/Program.cs
@@ -3,42 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace Calculator
+ namespace Calculator
 {
     class Program
     {
-        private static double number1;
-        private static double number2;
+        private static double num1;
+        private static double num2;
         private static double total;
         private static char oper;
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
             Console.WriteLine("Enter the first number ");
-            number1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter operator ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine("Enter operator ");
             oper = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine("Enter the second number ");
-            number2 = Convert.ToDouble(Console.ReadLine());
-            if (oper == '+') //оператор сложения
+             Console.WriteLine("Enter the second number ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+             if (oper == '+')
             {
-                total = number1 + number2;
+                total = num1 + num2;
                 Console.WriteLine("The result of addition equals " + total);
             }
             else if (oper == '-')
             {
-                total = number1 - number2;
-                Console.WriteLine("The result of the subtraction equals " + number1 + " - " + number2 + " = " + total);
+                total = num1 - num2;
+                Console.WriteLine("The result of the subtraction equals " + total);
             }
             else if (oper == '*')
             {
-                total = number1 * number2;
+                total = num1 * num2;
                 Console.WriteLine("The result of multiplying equals " + total);
             }
             else if (oper == '/')
             {
-                if (number2 == 0) { Console.WriteLine("Division by zero "); }
-                total = number1 / number2;
-                Console.WriteLine("The division result equals " + number1 + "/" + number2 + "=" + total);
+                if (num2 == 0) { Console.WriteLine("Division by zero "); }
+                total = num1 / num2;
+                Console.WriteLine("The division result equals " + total);
             }
             else
             { Console.WriteLine("Unknown operator "); }
