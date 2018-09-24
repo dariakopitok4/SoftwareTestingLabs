7b8f3eb7bddae1b9f49bcb7794b93f0c45cdc599
Darya Kopytok <dariakopitok4@gmail.com>
2018-09-24 21:01:27 +0300

    Change output of results

--- git/Lab1/Lab1/Program.cs
+++ git/Lab1/Lab1/Program.cs
@@ -38,7 +38,7 @@ namespace Calculator
             {
                 if (num2 == 0) { Console.WriteLine("Division by zero "); }
                 total = num1 / num2;
-                Console.WriteLine("The division result equals " + total);
+                Console.WriteLine("The division result equals " + number1 + "/" + number2 + "=" + total);
             }
             else
             { Console.WriteLine("Unknown operator "); }
