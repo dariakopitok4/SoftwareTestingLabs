88bcc01305d01fac178c7d0fc2cf037d5e10efe0
Darya Kopytok <dariakopitok4@gmail.com>
2018-10-17 10:43:54 +0300

    Add WebDriver task

---
+++ UnitTests/Triangle1/Triangle1/TriangTest/TestClass.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Triangle1
+{
+    [TestFixture]
+    class TestClass
+    {
+
+        Triangle_c t = new Triangle_c();
+
+        [Test]
+        public void Test_c_0_returnfalse()
+        {
+            Assert.AreEqual(false, t.Triangle(3, 5, 0));
+        }
+
+        [Test]
+        public void Test_abc_000_returnfalse()
+        {
+            Assert.AreEqual(false, t.Triangle(0, 0, 0));
+        }
+
+        [Test]
+        public void Test_a_0_returnfalse()
+        {
+            Assert.AreEqual(false, t.Triangle(0, 5, 5));
+        }
+
+        [Test]
+        public void Test_b_0_returnfalse()
+        {
+            Assert.AreEqual(false, t.Triangle(8, 0, 15));
+        }
+
+        [Test]
+        public void Test_b_minus9_returnfalse()
+        {
+            Assert.AreEqual(false, t.Triangle(5, -9, 15));
+        }
+
+        [Test]
+        public void Test_c_minus17_returnfalse()
+        {
+            Assert.AreEqual(false, t.Triangle(5, 9, -17));
+        }
+
+        [Test]
+        public void Test_abc_minus5minus9minus15_returnfalse()
+        {
+            Assert.AreEqual(false, t.Triangle(-5, -9, -15));
+        }
+
+        [Test]
+        public void Test_abc_minus5_returnfalse()
+        {
+            Assert.AreEqual(false, t.Triangle(-5, 9, 15));
+        }
+
+        [Test]
+        public void Test_ab_c_66_13_returnfalse()
+        {
+            Assert.AreEqual(false, t.Triangle(6, 6, 13));
+        }
+
+        [Test]
+        public void Test_abc_662_returnfalse()
+        {
+            Assert.AreEqual(true, t.Triangle(6, 6, 2));
+        }
+    }
+}
---
+++ WebDriverTests/WebDriverTests/WebDriverTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+namespace WebDriverTests
+
+{
+    [TestClass]
+    public class WebDriverTest
+    {
+        [TestMethod]
+        public void Test_enter_only_one_destination()
+        {
+            
+            RemoteWebDriver Driver = new ChromeDriver();
+           var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30)); 
+            Driver.Navigate().GoToUrl( @"https://www.vueling.com/ru");
+            string classname = @"form - input origin";
+            wait.Until(ExpectedConditions.ElementExists(By.ClassName(classname)));
+            var txtFirstParm = Driver.FindElementByClassName(classname);
+            var txtSecondParm = Driver.FindElementByClassName(@"form-input destination");
+            var but = Driver.FindElementById("btnSubmitHomeSearcher");
+            txtFirstParm.Clear();
+            txtFirstParm.SendKeys("Almeria");
+
+            txtSecondParm.Clear();
+
+            but.Click();
+        }
+    }
+}
