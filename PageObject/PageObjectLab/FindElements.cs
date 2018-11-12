bef017976993e1a4d341d6ab2ac2d5c3eac37915
Darya Kopytok <dariakopitok4@gmail.com>
2018-11-12 14:15:44 +0300

    Debug projects

---
+++ PageObject/PageObjectLab/FindElements.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+namespace WebDriverT
+{
+    class FindElements
+    {
+        /// <summary>Строка для ввода текста поиска</summary>
+        [FindsBy(How = How.ClassName, Using = "form-input origin")]
+        public IWebElement destinationBox;
+        [FindsBy(How = How.ClassName, Using = "form-input destination")]
+        public IWebElement departureBox;
+        [FindsBy(How = How.Id, Using = "btnSubmitHomeSearcher")]
+        public IWebElement findButton;
+        [FindsBy(How = How.Id, Using = "btnSubmitHomeSearcher")]
+        public IWebElement errorMess;
+    }
+}
---
+++ PageObject/PageObjectLab/PageObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Security.Policy;
+using System.Threading;
+
+namespace WebDriverT
+{
+    class Testclass
+    {
+        public IWebDriver driver;
+        string city = "Madrid";
+        string notFind = "Вы должны выбрать один из вариантов";
+
+        [Test]
+        public void page()
+        {
+            driver.Navigate().GoToUrl("https://www.vueling.com/ru");
+            FindElements pageHome = new FindElements(); // чтобы могли обращаться к объектам из PageHome.cs
+            PageFactory.InitElements(driver, pageHome); // инициализация элементов Page Object из PageHome.cs
+            pageHome.destinationBox.SendKeys(city);
+            pageHome.departureBox.SendKeys(city);
+            pageHome.findButton.Click();
+
+            Assert.AreEqual(notFind, pageHome.errorMess.Text);
+
+        }
+    }
+}
